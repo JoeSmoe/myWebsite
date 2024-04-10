63e335ae0d8f597a251cfe5150de12cc74cafd99
JoeSmoe <164091156+JoeSmoe@users.noreply.github.com>
2024-04-10 00:17:26 +0000

    Added JS

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+function writeCalendar(who) {
+  var target = document.getElementById('info');
+  var content = "";
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+
+  content = dd + "/" + mm + "/" + today.getFullYear()+"<br>";
+
+  switch(who) {
+    case "Joe":{
+      content = content + "Get Joe's calendar from database and display here.";
+      break;
+    }
+      case "Janet":{
+        content = content + "Get Janet's calendar from database and display here.";
+        break;
+      }
+  }
+
+  target.innerHTML = content;
+}
+
+
 //Weather Code from https://weatherwidget.io
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
