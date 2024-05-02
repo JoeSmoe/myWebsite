cf11be8040b47e3b92b3a539a82c2b72dadc1572
JoeSmoe <164091156+JoeSmoe@users.noreply.github.com>
2024-05-02 20:42:03 +0000

    added do list

--- form.js
+++ form.js
@@ -1,4 +1,57 @@
-function submitDoListItem() {
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCFgKcsEP0lnZdpZoD0tVCMoCw9NPC4e7Q",
+    authDomain: "dolist-dcede.firebaseapp.com",
+    projectId: "dolist-dcede",
+    storageBucket: "dolist-dcede.appspot.com",
+    messagingSenderId: "535764749530",
+    appId: "1:535764749530:web:0071534fd2dac708f0a9e3"
+  };
+
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  console.log("firebase setup complete!");
+
+function findDoItem() {
+  // get Do Item
+  var title = prompt("Enter a Do Item Title to find:");
+  db.collection("doList").doc(title).get().then((doc) => {
+    if (doc.exists) {
+      var theData = doc.data();
+      document.getElementById('message').innerHTML = theData.title + " " + theData.description + " " + theData.due;
+    } else {
+      document.getElementById('message').innerHTML = "No such Do Item found!";
+    }
+  })
+  .catch((err) => {
+    console.log('Do Item not found', err);
+    document.getElementById('message').innerHTML = "Do Item not found.";
+  });
+}
+
+function editDoItem() {
+  // get Do Item title
+  var title = prompt("Enter a Do Item Title to find:");
+  db.collection("doList").doc(title).get().then((doc) => {
+    if (doc.exists) {
+      var theData = doc.data();
+      document.getElementById('who').value = theData.who;
+      document.getElementById('title').value = theData.title;
+      document.getElementById('description').value = theData.description;
+      document.getElementById('due').value = theData.due;
+    } else {
+      document.getElementById('message').innerHTML = "No such Do Item found!";
+    }
+  })
+  .catch((err) => {
+    console.log('Do Item not found', err);
+    document.getElementById('message').innerHTML = "Do Item not found.";
+  });
+}
+
+function createDoItem() {
+  var who = document.getElementById('who').value;
   var title = document.getElementById('title').value;
   var description = document.getElementById('description').value;
   var due = document.getElementById('due').value;
@@ -9,18 +62,35 @@ function submitDoListItem() {
   var dueDateTime = new Date(dueDateTimeString);
   var dueTimestamp = dueDateTime.getTime();
   var doItem = {
+    who: who,
     title: title,
     description: description,
     due: dueTimestamp
   };
-  var doItemJSON = JSON.stringify(doItem);
-  console.log("doItemJSON: " + doItemJSON);
-  return doItemJSON;
-  //var doItemRef = db.collection("doItems").doc();
-  //doItemRef.set({
-    //doItemJSON: doItemJSON
-  //});
-  //document.getElementById("title").value = "";
-  //document.getElementById("description").value = "";
-  //document.getElementById("due").value = "";
+  return doItem;
+}
+
+function saveDoItem() {
+  // save to DB
+  var doItemData = createDoItem();
+  db.collection("doList").doc(doItemData.title).set(doItemData);
+  alert(doItemData.title + " saved to database!");
+}
+
+function deleteDoItem() {
+  // get Do Item title
+  var title = prompt("Enter a Do Item Title to find:");
+  db.collection("doList").doc(title).get().then((doc) => {
+    if (doc.exists) {
+      var theData = doc.data();
+      db.collection("doList").doc(title).delete();
+      document.getElementById('message').innerHTML = theData.title + "  successfully deleted.";
+    } else {
+      document.getElementById('message').innerHTML = "No such Do Item found!";
+    }
+  })
+  .catch((err) => {
+    console.log('Do Item not found', err);
+    document.getElementById('message').innerHTML = "Do Item not found.";
+  });
 }
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function writeCalendar(who) {
+
+function writeDoListOLD(who) {
   var target = document.getElementById('info');
   var content = "";
   var today = new Date();
@@ -21,6 +22,43 @@ function writeCalendar(who) {
   target.innerHTML = content;
 }
 
+// Do Items
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCFgKcsEP0lnZdpZoD0tVCMoCw9NPC4e7Q",
+    authDomain: "dolist-dcede.firebaseapp.com",
+    projectId: "dolist-dcede",
+    storageBucket: "dolist-dcede.appspot.com",
+    messagingSenderId: "535764749530",
+    appId: "1:535764749530:web:0071534fd2dac708f0a9e3"
+  };
+
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  console.log("firebase setup complete!");
+
+function writeDoList(who) {
+  db.collection("doList").where("who", "==", who).get().then((doc) => {
+    if (doc.exists) {
+      var theData = doc.data();
+      var content = "";
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1;
+      var yyyy = today.getFullYear();
+      content = dd + "/" + mm + "/" + yyyy + "<br>";
+      for (var i = 0; i < theData.length; i++) {
+        var doItem = theData[i];
+        content = content + doItem.title + " " + doItem.description + " " + doItem.due + "<br>";
+      }
+      document.getElementById('info').innerHTML = content;
+    } else {
+      document.getElementById('info').innerHTML = "No Do Items found!";
+    }
+  }
+  //return "HELLO";
+}
 
 //Weather Code from https://weatherwidget.io
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
